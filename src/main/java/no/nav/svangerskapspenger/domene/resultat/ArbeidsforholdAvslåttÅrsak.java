758c961ba789626944b6d1e5a33f6f8249306f48
Frode Lindås <frode.lindas@nav.no>
2019-03-14 14:14:12 +0100

    PFP-5951 fikset en skrivefeil og gjort arbeidsforholdId optional

--- src/main/java/no/nav/svangerskapspenger/domene/resultat/ArbeidsforholdAvslåttÅrsak.java
+++ src/main/java/no/nav/svangerskapspenger/domene/resultat/ArbeidsforholdAvslåttÅrsak.java
@@ -4,7 +4,7 @@ public enum ArbeidsforholdAvslåttÅrsak implements Årsak {
 
     LEGES_DATO_IKKE_FØR_TRE_UKER_FØR_TERMINDATO(8301, "Lege/jordmors dato for tilpassing er ikke før tre uker før termindato"),
     UTTAK_KUN_PÅ_HELG(8302, "Uttak kun på helg"),
-    ARBEIDSGIVER_KAN_TILRETTELEGGE(8303, "Abeidsgiver kan tilrettelegge");
+    ARBEIDSGIVER_KAN_TILRETTELEGGE(8303, "Arbeidsgiver kan tilrettelegge");
 
     private final int id;
     private final String beskrivelse;
