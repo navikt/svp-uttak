130c2b643caa37cb4188be85588d31fa7902b67d
Frode Lindås <frode.lindas@nav.no>
2019-03-26 10:29:00 +0100

    PFP-6560 la til enhetstester for overlapp logikk

--- src/test/java/no/nav/svangerskapspenger/regler/fastsettperiode/grunnlag/LukketPeriodeTest.java
+++ src/test/java/no/nav/svangerskapspenger/regler/fastsettperiode/grunnlag/LukketPeriodeTest.java
@@ -1,9 +1,9 @@
 package no.nav.svangerskapspenger.regler.fastsettperiode.grunnlag;
 
-
 import java.time.LocalDate;
 import java.time.Month;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class LukketPeriodeTest {
@@ -23,5 +23,36 @@ public class LukketPeriodeTest {
         new LukketPeriode(LocalDate.of(2019, Month.FEBRUARY, 1), LocalDate.of(2019, Month.JANUARY, 1)) {};
     }
 
+    @Test
+    public void ikke_overlapp_når_perioder2_er_etter_periode1() {
+        var periode1 = new LukketPeriode(LocalDate.of(2019, Month.JANUARY, 1), LocalDate.of(2019, Month.JANUARY, 31)) {};
+        var periode2 = new LukketPeriode(LocalDate.of(2019, Month.FEBRUARY, 1), LocalDate.of(2019, Month.FEBRUARY, 28)) {};
+
+        Assertions.assertThat(periode1.overlapper(periode2)).isFalse();
+    }
+
+    @Test
+    public void ikke_overlapp_når_perioder2_er_før_periode1() {
+        var periode1 = new LukketPeriode(LocalDate.of(2019, Month.FEBRUARY, 1), LocalDate.of(2019, Month.FEBRUARY, 28)) {};
+        var periode2 = new LukketPeriode(LocalDate.of(2019, Month.JANUARY, 1), LocalDate.of(2019, Month.JANUARY, 31)) {};
+
+        Assertions.assertThat(periode1.overlapper(periode2)).isFalse();
+    }
+
+    @Test
+    public void delvis_overlapp_tolkes_som_overlapp() {
+        var periode1 = new LukketPeriode(LocalDate.of(2019, Month.JANUARY, 1), LocalDate.of(2019, Month.FEBRUARY, 10)) {};
+        var periode2 = new LukketPeriode(LocalDate.of(2019, Month.FEBRUARY, 1), LocalDate.of(2019, Month.FEBRUARY, 28)) {};
+
+        Assertions.assertThat(periode1.overlapper(periode2)).isTrue();
+    }
+
+    @Test
+    public void helt_overlapp_tolkes_som_overlapp() {
+        var periode1 = new LukketPeriode(LocalDate.of(2019, Month.JANUARY, 1), LocalDate.of(2019, Month.FEBRUARY, 10)) {};
+        var periode2 = new LukketPeriode(LocalDate.of(2019, Month.JANUARY, 1), LocalDate.of(2019, Month.FEBRUARY, 10)) {};
+
+        Assertions.assertThat(periode1.overlapper(periode2)).isTrue();
+    }
 
 }
