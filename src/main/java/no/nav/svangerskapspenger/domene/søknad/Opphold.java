76a4b9c6ebddc71f3bea4f7eaaa84664ce707411
anjaaalerud <anja.gotesen.aalerud@nav.no>
2023-08-15 16:28:55 +0200

    TFP-5544 Opphold pga sykepenger skal ikke påvirkes av helligdager

--- src/main/java/no/nav/svangerskapspenger/domene/søknad/Opphold.java
+++ src/main/java/no/nav/svangerskapspenger/domene/søknad/Opphold.java
@@ -24,23 +24,27 @@ public class Opphold extends LukketPeriode {
     public static List<Opphold> opprett(LocalDate fom, LocalDate tom, SvpOppholdÅrsak årsak) {
         var oppholdPerioder = new ArrayList<Opphold>();
         var opppholdPeriode = new Opphold(fom, tom, årsak);
-        var helligdager = BevegeligeHelligdagerUtil.finnBevegeligeHelligdagerUtenHelg(opppholdPeriode);
-        for (LocalDate helligdag : helligdager) {
-            if (opppholdPeriode.overlapper(helligdag)) {
-                var minstToOppholdDager = opppholdPeriode.getFom().isBefore(opppholdPeriode.getTom());
-                if (!minstToOppholdDager) {
-                    //Bare en dag, og det er helligdag.
-                    return Collections.emptyList();
-                } else if (helligdag.equals(opppholdPeriode.getFom())) {
-                    //Helligdag på første dag i oppholdet, kutt en dag i starten av oppholdet.
-                    opppholdPeriode = new Opphold(opppholdPeriode.getFom().plusDays(1), opppholdPeriode.getTom(), årsak);
-                } else if (helligdag.equals(opppholdPeriode.getTom())) {
-                    //Helligdag på siste dag i oppholdet, kutt en dag i slutten av ophholdet. Resten av helligdagene må være etter oppholdet, og avbryter derfor loopen.
-                    opppholdPeriode = new Opphold(opppholdPeriode.getFom(), opppholdPeriode.getTom().minusDays(1), årsak);
-                    break;
-                } else {
-                    oppholdPerioder.add(new Opphold(opppholdPeriode.getFom(), helligdag.minusDays(1), årsak));
-                    opppholdPeriode = new Opphold(helligdag.plusDays(1), opppholdPeriode.getTom(), årsak);
+
+        //Når det er opphold pga annen ytelse (sykepenger)skal vi ikke ta hensyn til bevegelige helligdager
+        if (SvpOppholdÅrsak.FERIE.equals(årsak)) {
+            var helligdager = BevegeligeHelligdagerUtil.finnBevegeligeHelligdagerUtenHelg(opppholdPeriode);
+            for (LocalDate helligdag : helligdager) {
+                if (opppholdPeriode.overlapper(helligdag)) {
+                    var minstToOppholdDager = opppholdPeriode.getFom().isBefore(opppholdPeriode.getTom());
+                    if (!minstToOppholdDager) {
+                        //Bare en dag, og det er helligdag.
+                        return Collections.emptyList();
+                    } else if (helligdag.equals(opppholdPeriode.getFom())) {
+                        //Helligdag på første dag i oppholdet, kutt en dag i starten av oppholdet.
+                        opppholdPeriode = new Opphold(opppholdPeriode.getFom().plusDays(1), opppholdPeriode.getTom(), årsak);
+                    } else if (helligdag.equals(opppholdPeriode.getTom())) {
+                        //Helligdag på siste dag i oppholdet, kutt en dag i slutten av ophholdet. Resten av helligdagene må være etter oppholdet, og avbryter derfor loopen.
+                        opppholdPeriode = new Opphold(opppholdPeriode.getFom(), opppholdPeriode.getTom().minusDays(1), årsak);
+                        break;
+                    } else {
+                        oppholdPerioder.add(new Opphold(opppholdPeriode.getFom(), helligdag.minusDays(1), årsak));
+                        opppholdPeriode = new Opphold(helligdag.plusDays(1), opppholdPeriode.getTom(), årsak);
+                    }
                 }
             }
         }
